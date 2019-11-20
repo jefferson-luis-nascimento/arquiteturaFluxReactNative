039c5ab874a0ca5b04c09f1fad4aa754cd29552a
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-11-20 10:39:26 -0200

    Iniciando do desenvolvimento

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,102 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-import { Container } from './styles';
+import {
+  Container,
+  ProductList,
+  ProductItem,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  AddButtonView,
+  AddButtonText
+} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <View />
-    </Container>
-  );
+// import api from '../../services/api';
+
+export default class Main extends Component {
+  state = {
+    products: []
+  };
+
+  async componentDidMount() {
+    // const response = await api.get(`products`);
+
+    this.setState({
+      products: [
+        {
+          id: 1,
+          title: 'Tênis de Caminhada Leve Confortável',
+          price: 179.9,
+          image:
+            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg'
+        },
+        {
+          id: 2,
+          title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+          price: 139.9,
+          image:
+            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg'
+        },
+        {
+          id: 3,
+          title: 'Tênis Adidas Duramo Lite 2.0',
+          price: 219.9,
+          image:
+            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg'
+        },
+        {
+          id: 5,
+          title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+          price: 139.9,
+          image:
+            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg'
+        },
+        {
+          id: 6,
+          title: 'Tênis Adidas Duramo Lite 2.0',
+          price: 219.9,
+          image:
+            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg'
+        },
+        {
+          id: 4,
+          title: 'Tênis de Caminhada Leve Confortável',
+          price: 179.9,
+          image:
+            'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg'
+        }
+      ]
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <Container>
+        <ProductList
+          data={products}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <ProductItem>
+              <ProductImage source={{ uri: item.image }} />
+              <ProductTitle>{item.title}</ProductTitle>
+              <ProductPrice>{item.priceFormatted}</ProductPrice>
+              <AddButton>
+                <AddButtonView>
+                  <MdShoppingCart size={20} />
+                  <AddButtonText>Adicionar ao carrinho</AddButtonText>
+                </AddButtonView>
+              </AddButton>
+            </ProductItem>
+          )}
+        />
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
-  title: 'Olá Mundo'
+  title: 'Rocketshoes'
 };
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,68 @@
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
   background: #666;
+`;
+
+export const ProductList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  showsHorizontalScrollIndicator: false
+})`
+  margin-top: 20px;
+  background: #fff;
+`;
+
+export const ProductItem = styled.View`
+  flex: 1;
+  flex-direction: column;
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+`;
+export const ProductImage = styled.Image`
+  align-self: center;
+  width: 250px;
+  height: 250px;
+  border-radius: 4px;
+  background: #eee;
+`;
+export const ProductTitle = styled.Text`
+  font-size: 16px;
+  line-height: 20px;
+  color: #333;
+  margin-top: 5px;
+  font-weight: bold;
+`;
+export const ProductPrice = styled.Text`
+  font-size: 21px;
+  font-weight: bold;
+  margin: 5px 0 20px;
+`;
+export const AddButton = styled(RectButton)`
+  background: #7159c1;
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+  overflow: hidden;
+  margin-top: auto;
+  display: flex;
+  align-items: center;
+`;
+
+export const AddButtonView = styled.View`
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  padding: 12px;
+  background: rgba(0, 0, 0, 0.1);
+`;
+
+export const AddButtonText = styled.Text`
+  flex: 1;
+  text-align: center;
+  font-weight: bold;
+  color: #fff;
 `;
