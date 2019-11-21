d0770a06e4147117699fa71030c1acbbc531c349
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2019-11-21 16:17:12 -0200

    Cração da tela de carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,109 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  CartList,
+  CartItem,
+  CartItemDetails,
+  ItemImage,
+  ItemDetails,
+  ItemTitle,
+  ItemPrice,
+  RemoveItemButton,
+  CartItemTotals,
+  AddAmountButton,
+  AmountInput,
+  SubtractAmountButton,
+  CartItemAmount,
+  CartItemSubtotal,
+  Total,
+  TotalText,
+  TotalValue,
+  FinalizeOrder,
+  FinalizeOrderText,
+  EmptyCartText,
+} from './styles';
 
-export default function Cart() {
-  return <View />;
+export default class Cart extends Component {
+  state = {
+    cart: [
+      /* {
+        id: 2,
+        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+        price: 139.9,
+        priceFormatted: 'R$ 139,90',
+        amount: 2,
+        subtotal: 279.8,
+        subtotalFormatted: 'R$ 279,80',
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+      }, */
+    ],
+  };
+
+  render() {
+    const { cart } = this.state;
+    console.tron.log(cart);
+    return (
+      <Container>
+        {cart.length === 0 ? (
+          <>
+            <Icon name="remove-shopping-cart" size={48} color="#c1c1c1" />
+            <EmptyCartText>Seu carrinho está vazio.</EmptyCartText>
+          </>
+        ) : (
+          <>
+            <CartList
+              data={cart}
+              keyExtractor={product => product.id}
+              renderItem={({ item }) => (
+                <CartItem>
+                  <CartItemDetails>
+                    <ItemImage source={{ uri: item.image }} />
+                    <ItemDetails>
+                      <ItemTitle>{item.title}</ItemTitle>
+                      <ItemPrice>{item.priceFormatted}</ItemPrice>
+                    </ItemDetails>
+                    <RemoveItemButton onPress={() => {}}>
+                      <Icon name="delete-forever" color="#7159c1" size={24} />
+                    </RemoveItemButton>
+                  </CartItemDetails>
+                  <CartItemTotals>
+                    <CartItemAmount>
+                      <AddAmountButton onPress={() => {}}>
+                        <Icon
+                          name="add-circle-outline"
+                          color="#7159c1"
+                          size={24}
+                        />
+                      </AddAmountButton>
+                      <AmountInput value={String(item.amount)} />
+                      <SubtractAmountButton onPress={() => {}}>
+                        <Icon
+                          name="remove-circle-outline"
+                          color="#7159c1"
+                          size={24}
+                        />
+                      </SubtractAmountButton>
+                    </CartItemAmount>
+                    <CartItemSubtotal>
+                      {item.subtotalFormatted}
+                    </CartItemSubtotal>
+                  </CartItemTotals>
+                </CartItem>
+              )}
+            />
+            <Total>
+              <TotalText>Total</TotalText>
+              <TotalValue>R$ 690,80</TotalValue>
+              <FinalizeOrder>
+                <FinalizeOrderText>FinalizarPedido</FinalizeOrderText>
+              </FinalizeOrder>
+            </Total>
+          </>
+        )}
+      </Container>
+    );
+  }
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,5 +1,150 @@
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  margin: 20px;
+  padding: 10px;
+  background: #fff;
+  flex-direction: column;
+  border-radius: 4px;
+  align-items: center;
+`;
 
+export const CartList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  showsHorizontalScrollIndicator: false,
+})`
+  border-radius: 4px;
+`;
+
+export const CartItem = styled.View`
+  flex-direction: column;
+  background: #fff;
+  border-radius: 4px;
+  padding: 10px;
+  margin: 10px;
+  flex: 1;
+`;
+
+export const CartItemDetails = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ItemImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  align-content: center;
+`;
+
+export const ItemDetails = styled.View`
+  flex: 1;
+  justify-content: flex-start;
+`;
+
+export const ItemTitle = styled.Text`
+  margin-left: 10px;
+  text-align: left;
+  font-size: 16px;
+`;
+
+export const ItemPrice = styled.Text`
+  margin-left: 10px;
+  text-align: left;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const RemoveItemButton = styled.TouchableOpacity`
+  border-radius: 4px;
+  width: 28px;
+  height: 28px;
+`;
+
+export const CartItemTotals = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: #e9e9e9;
+  border-radius: 4px;
+  padding: 10px;
+`;
+
+export const CartItemAmount = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const AddAmountButton = styled.TouchableOpacity`
+  border-radius: 4px;
+  width: 28px;
+  height: 28px;
+`;
+
+export const AmountInput = styled.TextInput.attrs({
+  readonly: true,
+})`
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  color: #666;
+  padding: 6px;
+  width: 50px;
+  background: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  margin-right: 10px;
+`;
+export const SubtractAmountButton = styled.TouchableOpacity`
+  border-radius: 4px;
+  width: 28px;
+  height: 28px;
+`;
+
+export const CartItemSubtotal = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const Total = styled.View`
+  margin: 10px;
+  background: #fff;
+  height: 300px;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TotalText = styled.Text`
+  font-size: 20px;
+  text-transform: uppercase;
+  color: #666;
+`;
+export const TotalValue = styled.Text`
+  font-size: 36px;
+  font-weight: bold;
+`;
+export const FinalizeOrder = styled(RectButton)`
+  margin-top: 20px;
+  border: 0;
+  border-radius: 4px;
+  background: #7159c1;
+  height: 60px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+export const FinalizeOrderText = styled.Text`
+  color: #fff;
+  font-size: 24px;
+`;
+
+export const EmptyCartText = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
 `;
